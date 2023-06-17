80a0c46d279c5beba2cab5a7ab0e7d72528afd49
AVIV <avivhertzman@gmail.com>
2023-06-18 00:22:24 +0300

    reading all data

--- csv_manager.py
+++ csv_manager.py
@@ -2,6 +2,6 @@ from constant import FILE_PATH
 import pandas as pd
 
 def get_data():
-    return pd.read_csv(FILE_PATH, nrows=30)
+    return pd.read_csv(FILE_PATH)
 def update_data(updated_data):
     updated_data.to_csv(FILE_PATH, index=False, mode='w')
